041db3cb65efcf23d20a316ed5623d5a23645822
Devin Molnau <demolnau@iastate.edu>
2017-04-19 17:53:58 -0500

    alterations to R-script

--- R_script_devin.R
+++ R_script_devin.R
@@ -6,11 +6,17 @@ biocLite("edgeR")
 library(edgeR)
 counts_data<-read.delim("Asparagus.RSEM_genecounts.txt", check.names=FALSE, stringsAsFactors = FALSE) #read in gene counts
 head(counts_data)
+#group<-c(Line_8A,Line_8A,Line_8A,Line_8B,Line_8B,Line_8B,Line_10, Line_10, Line_10, Line_9, Line_9)
 cd<-DGEList(counts=counts_data) #convert gene count data to DGEList
-keep <- rowSums(cpm(cd)>1) >= 2 #filters out lowly expressed genes
+keep <- rowSums(cpm(cd)>1) >= 3 #filters out lowly expressed genes
 cd <- cd[keep, , keep.lib.sizes=FALSE]
 cd<-calcNormFactors(cd) #normalizes
-plotMDS(cd)
+#line<-factor(c(88,88,88,89,89,89,103,103,103,9,9))
+colors <- c("black","black","black","darkgreen","darkgreen","darkgreen","blue","blue","blue", "red", "red")
+names<-c("female", "male","supermale","female", "male","supermale","female", "male")
+plotMDS(cd, col=colors, labels=names, top = 250, gene.selection = "pairwise")
+legend("center",legend=c("Line_8A", "Line_8B","Line_10", "Line_9"), col= c("black","darkgreen","blue", "red"), ncol=2, title="Key" )
+
 
 #design matrix of sex and line
 rm(design)
@@ -36,10 +42,12 @@ fit <- glmQLFit(cd, design)
 plotQLDisp(fit)
 
 qlf<-glmQLFTest(fit,coef=2:3)
+topTags(qlf)
 plotMD(qlf)
+abline(h=c(-1, 1), col="blue")
 
 tr<-glmTreat(fit) 
-plotMDS()
+plotMDS(tr)
 logcpm <- cpm(y, prior.count=2, log=TRUE) #for heat map
 
 cd
