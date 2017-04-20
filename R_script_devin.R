038e8fe99ef4e25651fe485a032d037be90d5790
demolnau <demolnau@iastate.edu>
2017-04-20 13:54:32 -0500

    Update R_script_devin.R

--- R_script_devin.R
+++ R_script_devin.R
@@ -15,7 +15,7 @@ cd<-calcNormFactors(cd) #normalizes
 colors <- c("black","black","black","darkgreen","darkgreen","darkgreen","blue","blue","blue", "red", "red")
 names<-c("female", "male","supermale","female", "male","supermale","female", "male")
 plotMDS(cd, col=colors, labels=names, top = 250, gene.selection = "pairwise")
-legend("center",legend=c("Line_8A", "Line_8B","Line_10", "Line_9"), col= c("black","darkgreen","blue", "red"), ncol=2, title="Key" )
+#legend('center',legend=c("Line_8A", "Line_8B","Line_10", "Line_9"), col= c("black","darkgreen","blue", "red"), ncol=2, title="Key" )
 
 
 #design matrix of sex and line
@@ -31,7 +31,7 @@ design
 rm(design)
 sex<-factor(c("XX","XY","YY","XX","XY","YY","XX","XY","YY","XX","XY"))
 data.frame(Sample=colnames(cd),sex)
-design<-model.matrix(~sex)
+design<-model.matrix(~0+sex)
 rownames(design) <- colnames(cd)
 design
 
@@ -41,14 +41,27 @@ cd <- estimateDisp(cd, design)
 fit <- glmQLFit(cd, design)
 plotQLDisp(fit)
 
-qlf<-glmQLFTest(fit,coef=2:3)
+qlf<-glmQLFTest(fit,coef=5:6)
+FDR<-p.adjust(qlf$table$PValue, method="BH")
+sum(FDR<0.05)
+summary(decideTests(qlf))
+
+
+qlf<-glmQLFTest(fit, contrast=c(0,0,0,-1,1)) #male VS supermale
+
 topTags(qlf)
 plotMD(qlf)
-abline(h=c(-1, 1), col="blue")
 
-tr<-glmTreat(fit) 
-plotMDS(tr)
-logcpm <- cpm(y, prior.count=2, log=TRUE) #for heat map
+qlf2<-glmQLFTest(fit, coef=5) #female vs male
+FDR2<-p.adjust(qlf2$table$PValue, method="BH")
+sum(FDR<0.05)
+topTags(qlf2)
+plotMD(qlf2)
 
-cd
-et
+qlf3<-glmQLFTest(fit, coef=6)
+FDR3<-p.adjust(qlf3$table$PValue, method="BH")
+sum(FDR<0.05)
+topTags(qlf3)
+plotMD(qlf3)
+
+
